4a88646a54a24295f9d92e231dd97256d8f8a86f
Hanze Liu <liuhanzecrystal@yahoo.com>
2024-11-26 23:43:38 -0500

    width

--- src/api/apiConfigs.js
+++ src/api/apiConfigs.js
@@ -1,3 +1,3 @@
 //export const API_BASE_URL = "https://api-node-4-d27t.onrender.com"; // named export
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; // named export
-console.log(import.meta.env.VITE_API_BASE_URL);
+//console.log(import.meta.env.VITE_API_BASE_URL);
